8960cd7639e777dccb9e108163dd4767fb2471f5
JeanDavid26 <duvernayjeandavid@gmail.com>
2025-10-25 12:06:04 +0200

    logo dashboard

--- front/src/app/private/dashboard/pages/dashboard-detail/dashboard-detail.component.ts
+++ front/src/app/private/dashboard/pages/dashboard-detail/dashboard-detail.component.ts
@@ -14,6 +14,7 @@ import Decimal from 'decimal.js';
   styleUrl: './dashboard-detail.component.scss'
 })
 export class DashboardDetailComponent {
+  currentDate: Date = new Date();
 
   totalArticles: number = 0;
   totalInventory: number = 0;
